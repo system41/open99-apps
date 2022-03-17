6425fa68a4dfc9d77ca995c22899a9e2e395a1d6
its-pablo <78528552+its-pablo@users.noreply.github.com>
2022-03-16 18:32:28 -0700

    Chromium (taken from daedalOS)

---
+++ chromium/index.js
@@ -0,0 +1,50 @@
+"use strict";
+
+let bookmarks = [
+    {
+        icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/150px-Google_%22G%22_Logo.svg.png",
+        name: "Google",
+        url: "https://www.google.com/webhp?igu=1",
+    },
+    {
+        icon: "/favicon.ico",
+        name: "daedalOS",
+        url: "https://dustinbrett.com/",
+    },
+    {
+        icon: "/System/Icons/win96.png",
+        name: "Windows 96",
+        url: "https://windows96.net/",
+    },
+    {
+        icon: "/System/Icons/aos.png",
+        name: "AaronOS",
+        url: "https://aaronos.dev/AaronOS/",
+    },
+    {
+        icon: "/System/Icons/jspaint.png",
+        name: "JS Paint",
+        url: "https://jspaint.app/",
+    },
+    {
+        icon: "/System/Icons/minecraft.png",
+        name: "Minecraft Classic",
+        url: "https://classic.minecraft.net/",
+    },
+    {
+        icon: "/System/Icons/diablo.png",
+        name: "Diablo",
+        url: "https://d07riv.github.io/diabloweb/",
+    },
+];
+
+let app = document.getElementById('BROWSERapp')
+let bookmarkBar = app.children[1]
+
+bookmarks.forEach(function(item){
+    let btn = document.createElement('button')
+    btn.ariaLabel = item.name + " " + item.url
+    btn.title = item.name + " " + item.url
+    btn.type = "button"
+    bookmarkBar.appendChild(btn)
+})
